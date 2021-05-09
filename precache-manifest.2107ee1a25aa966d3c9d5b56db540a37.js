ef640868bd76b9ba6f12cda2cd70d05742c671d5
TANMAYBAGADIA <tanmaybagadia1301@gmail.com>
2021-05-09 13:04:54 +0530

    Updates

---
+++ precache-manifest.2107ee1a25aa966d3c9d5b56db540a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189770804ad2b506da1e8799d8e7fced",
+    "url": "/Animation-MothersDay/index.html"
+  },
+  {
+    "revision": "9524b4cf18f992cfeceb",
+    "url": "/Animation-MothersDay/static/css/main.ae92371a.chunk.css"
+  },
+  {
+    "revision": "737778a08d00e6cc9178",
+    "url": "/Animation-MothersDay/static/js/2.c9f6f1a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Animation-MothersDay/static/js/2.c9f6f1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9524b4cf18f992cfeceb",
+    "url": "/Animation-MothersDay/static/js/main.aa3b3949.chunk.js"
+  },
+  {
+    "revision": "0ce5c6c445d3977b25eb",
+    "url": "/Animation-MothersDay/static/js/runtime-main.7b65de10.js"
+  }
+]);
